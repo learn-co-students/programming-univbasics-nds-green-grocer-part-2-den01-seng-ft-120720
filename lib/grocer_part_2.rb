94c393307ff3a358cc6ee0d2171cb3834bd1b8e8
Colton O'Connor <colton.shawn.oconnor@gmail.com>
2020-11-15 17:54:13 +0000

    Done.

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -4,12 +4,46 @@ def apply_coupons(cart, coupons)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
+  coupons.each do |coupon|
+    cart.each do |inner_cart|
+      if inner_cart[:item] == coupon[:item] && inner_cart[:count] >= coupon[:num]
+        inner_cart[:count] = inner_cart[:count] - coupon[:num]
+        cart[cart.length()] = {
+          :item => "#{inner_cart[:item]} W/COUPON",
+          :price => coupon[:cost]/coupon[:num],
+          :clearance => inner_cart[:clearance],
+          :count => coupon[:num]
+        }
+      end
+      #binding.pry
+    end
+  end
+  cart
 end
 
 def apply_clearance(cart)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
+  
+  #output = []
+  
+  cart.each do |inner_cart|
+    if inner_cart[:clearance]
+      #output.push({
+        #:item => inner_cart[:item],
+        #:price => (inner_cart[:price]*0.8).round(2),
+        #:clearance => inner_cart[:clearance],
+        #:count => inner_cart[:count]
+      #})
+      inner_cart[:price] = (inner_cart[:price]*0.8).round(2)
+    #else
+      #output << inner_cart
+    end
+  end
+  #binding.pry
+  #output
+  cart
 end
 
 def checkout(cart, coupons)
@@ -22,4 +56,19 @@ def checkout(cart, coupons)
   #
   # BEFORE it begins the work of calculating the total (or else you might have
   # some irritated customers
+  
+  cart = consolidate_cart(cart)
+  cart = apply_coupons(cart, coupons)
+  cart = apply_clearance(cart)
+  #binding.pry
+  total = 0
+  
+  cart.each do |inner_cart|
+    total += (inner_cart[:count] * inner_cart[:price])
+  end
+  #binding.pry
+  if total > 100
+    total = (total * 0.90).round(2)
+  end
+  total
 end

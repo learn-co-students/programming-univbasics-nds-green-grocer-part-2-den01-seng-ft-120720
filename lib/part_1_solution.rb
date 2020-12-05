17911d0fa1c65a1abbb4dc7f4ebb0f67ef72f4d0
Jack Hubert <jphubert93@gmail.com>
2020-12-05 23:10:26 +0000

    Done.

--- lib/part_1_solution.rb
+++ lib/part_1_solution.rb
@@ -9,26 +9,19 @@ def find_item_by_name_in_collection(name, collection)
   nil
 end
 
-
 def consolidate_cart(cart)
-  index = 0
-  new_cart = []
+  number_items = 0
+  consolidated_cart = []
   
-  cart.each do |grocery_item|
-    current_item = find_item_by_name_in_collection(grocery_item[:item], new_cart)
-    if current_item
-      new_cart_index = 0
-      new_cart.each do |new_cart_item|
-        if new_cart_item[:item] === current_item[:item]
-          new_cart_item[:count] += 1
-        end
-        new_cart_index += 1
-      end
+  cart.each do |item|
+    if !consolidated_cart.include? item
+      item[:count] = 1  
+      consolidated_cart << item
     else
-      grocery_item[:count] = 1
-      new_cart << grocery_item
+      name = item[:item]
+      find_item_by_name_in_collection(name, consolidated_cart)[:count] += 1
     end
-    index += 1
   end
-  new_cart
+
+  consolidated_cart
 end
